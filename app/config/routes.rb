0051314d451ec29dabff9ad909c52b345938ca99
emisosnowsky <emiliasosnowska8@gmail.com>
2017-01-03 11:30:22 +0100

    dodanie ścieżek do strony

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
  	
  	get '/home', to: 'pages#home'
 
- end
+ 	get '/products', to: 'products#index'
+
+ 	get '/categories', to: 'categories#index', as: 'show_categories' 
+
+ 	get 'product/buy', to: 'product#buy', as: 'buy_product'
+
+ 	get 'products/:id', to: 'catalog#view'
+
+     get '/signup', to: 'customers#signup'
+     get '/login', to: 'customers#login'
+     get '/logout', to: 'customers#logout'
+
+end
