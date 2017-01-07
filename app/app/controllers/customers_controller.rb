31252376b761b9bc24a57934bfe39a361f3d51d4
emisosnowsky <emiliasosnowska8@gmail.com>
2017-01-07 16:38:17 +0100

    Dodanie rejestrowania na strone

--- app/app/controllers/customers_controller.rb
+++ app/app/controllers/customers_controller.rb
@@ -4,4 +4,22 @@ class CustomersController < ApplicationController
 		@customer = Customer.new
 	end
 
+	def create
+		@customer = Customer.new(customer_params)
+
+		if @customer.save
+			flash[:success] = "Rejestracja przebiegła pomyślnie. Jesteś zarejestrowany!"
+			redirect_to home_path
+		else
+			render :new
+		end
+	end
+
+	private 
+
+		def customer_params
+			params.require(:customer).permit(:login, :email, :password, :name, :lastname, :address, :phone)
+		end
+
+
 end
