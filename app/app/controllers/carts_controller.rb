9375d6d98c06c38d0eea48e4711023e7f282b384
Emilia <emiliasosnowska8@gmail.com>
2017-01-15 12:54:41 +0000

    Praca nad dodaniem do koszyka

--- app/app/controllers/carts_controller.rb
+++ app/app/controllers/carts_controller.rb
@@ -12,4 +12,24 @@ class CartsController < ApplicationController
 	def new 
 		@order = Order.new
 	end
+	
+	def create
+		@product = Product.find(params[:id])
+		@order = Order.new(order_params)
+		if @order.save
+			flash[:success] = "Utworzono zamówienie poprawnie."
+			redirect_to products_path
+		else
+			render :new
+		end
+	end
+	
+	private 
+
+	def order_params
+		params.require(:order).permit(:order_number, :totalprice, :payment_type)
+	end
+	
+
 end
+
